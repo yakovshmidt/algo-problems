890068c0779f71c1f54d2494feac68d4ac8a3dfc
Yakov Shmidt <yakovshmidt93@gmail.com>
2022-03-07 21:18:42 +0600

    feat: solve "3Sum" problem using array sorting algo

---
+++ src/main/java/kz/home/ys/algo/middle/threeSum/ThreeSumUsingInputArraySorting.java
@@ -0,0 +1,38 @@
+package kz.home.ys.algo.middle.threeSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ThreeSumUsingInputArraySorting {
+
+    // time - O(Nlog(N)) + O(N^2) -> O(N^2)
+    // space - O(N) - because sorting does take extra space
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+
+            if (i > 0 && first == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int second = nums[l];
+                int third = nums[r];
+                int sum = first + second + third;
+                if (sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    result.add(List.of(first, second, third));
+                    l++;
+                    while (nums[l] == nums[l - 1] && l < r) l++;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/kz/home/ys/algo/middle/threeSum/ThreeSumUsingInputArraySortingTest.java
@@ -0,0 +1,21 @@
+package kz.home.ys.algo.middle.threeSum;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThreeSumUsingInputArraySortingTest {
+
+    @Test
+    public void testThreeSum() {
+        List<List<Integer>> threeSum = new ThreeSumUsingInputArraySorting().threeSum(new int[]{-2, -2, 0, 0, 2, 2});
+
+        assertEquals(1, threeSum.size());
+        assertEquals(3, threeSum.get(0).size());
+        assertEquals(-2, threeSum.get(0).get(0).intValue());
+        assertEquals(0, threeSum.get(0).get(1).intValue());
+        assertEquals(2, threeSum.get(0).get(2).intValue());
+    }
+}
