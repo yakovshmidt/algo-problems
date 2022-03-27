1378b78810851f2f5b37e43863f866f44ba2af85
Yakov Shmidt <yakovshmidt93@gmail.com>
2022-03-27 10:35:47 +0600

    feat: solve "three number sort" problem using brute force approach

---
+++ src/test/java/kz/home/ys/algo/medium/threeNumberSort/ThreeNumberSortTest.java
@@ -0,0 +1,23 @@
+package kz.home.ys.algo.medium.threeNumberSort;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThreeNumberSortTest {
+
+    @Test
+    public void testThreeNumberSort() {
+        var array = new int[]{1, 0, 0, -1, -1, 0, 1, 1};
+        var order = new int[]{0, 1, -1};
+        var expected = new int[]{0, 0, 0, 1, 1, 1, -1, -1};
+
+        var actual = new ThreeNumberSort().threeNumberSort(array, order);
+
+        assertEquals(expected.length, actual.length);
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], actual[i]);
+        }
+    }
+}
